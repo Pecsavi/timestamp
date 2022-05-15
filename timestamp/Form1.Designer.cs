48747835ae1ff132d9b2dae1ffd09ff4be501c2b
Perényi Csaba <perenyicsaba@gmail.com>
2022-05-15 09:14:08 +0200

    Projektdateien hinzufügen.

---
+++ timestamp/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace timestamp
+{
+    partial class Timestamp
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoEllipsis = true;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(449, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(449, 43);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Timestamp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 86);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Timestamp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timestamp";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ timestamp/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace timestamp
+{
+
+
+    public partial class Timestamp : Form
+    {
+        int counter = 0;
+        string title="Log";
+        string text, alien;
+        Font font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        Label lbl;
+        DateTime time1, time2;
+        TimeSpan delta1, delta2;
+        Screen scr;
+
+        public Timestamp()
+        {
+            InitializeComponent();
+            //form = this;
+            //this.WindowState = FormWindowState.Maximized;
+            this.TopMost=true;
+            this.StartPosition = FormStartPosition.Manual;
+            scr = Screen.FromPoint(this.Location);
+            this.Location = new Point(scr.WorkingArea.Right - this.Width-20, scr.WorkingArea.Top);
+            label1.Text = (DateTime.Now.ToString("d"));
+            label1.Font=font;
+
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Export();
+            
+        }
+        
+        public void Export()
+        {
+            text = "";
+            button1.Text = "activity is in progress";
+            string Path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+ @"\time.txt";
+           
+            StreamWriter sw = new StreamWriter(Path , true);
+            if (counter==0)
+            { 
+             
+              sw.WriteLine(DateTime.Now.ToString("d"));
+            }
+            
+            alien = "in";
+
+            if (counter % 2 != 0)
+            {
+                button1.Text = "activity is stopped";
+             
+                
+                time2 = DateTime.Now;
+               
+                delta1 = time2.Subtract(time1);
+                delta2 = delta2.Add(delta1);
+                alien = "out - Sum:"+delta2.ToString(@"hh\:mm");
+            }
+            time1 = DateTime.Now;
+            InputBox("Log", "Description of the activity:", ref text);
+            string loginfo = time1.ToString("HH:mm:ss") +  "  -  " + text + " - " + "  -  " + alien;
+            sw.WriteLine(loginfo);
+
+            sw.Close();
+            Felrak(loginfo);
+            counter++;
+            
+        }
+        public void Felrak(string loginfo)
+        {
+
+            foreach (Control nagy in Controls)
+            {
+                foreach (Control elem in nagy.Controls)
+                {
+                    if (elem is Label)
+                    {
+                        elem.Location = new Point(elem.Location.X, elem.Location.Y + 25);
+                    }
+                }
+            }
+
+               
+
+            lbl = new Label();
+            lbl.AutoSize = true;
+            lbl.Font = font;
+            lbl.Location = new Point(12, 12);
+            lbl.Text = loginfo;
+            panel1.Controls.Add(lbl);
+            panel1.Height= panel1.Height + 25;
+
+
+        }
+
+        public static DialogResult InputBox(string title, string promptText, ref string value)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            label.Text = promptText;
+            textBox.Text = value;
+
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            label.SetBounds(9, 20, 372, 13);
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(228, 72, 75, 23);
+            buttonCancel.SetBounds(309, 72, 75, 23);
+
+            label.AutoSize = true;
+            textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            form.ClientSize = new Size(396, 107);
+           form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+          
+            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.Location = new Point(500, 500);
+   
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult dialogResult = form.ShowDialog();
+            
+            value = textBox.Text;
+
+
+            return dialogResult;
+        }
+
+        
+    }
+}
