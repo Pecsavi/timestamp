068e0c2cf7d6a787ca842ad033ec411aac5b35de
Perényi Csaba <perenyicsaba@gmail.com>
2022-10-17 21:45:48 +0200

    repair

--- timestamp/Form1.cs
+++ timestamp/Form1.cs
@@ -2,7 +2,7 @@
 using System.Drawing;
 using System.Windows.Forms;
 using System.IO;
-using System.Threading;
+
 
 
 namespace timestamp
@@ -11,24 +11,29 @@ namespace timestamp
 
     public partial class Timestamp : Form
     {
-        public event System.Timers.ElapsedEventHandler Elapsed;// ez a Jokerfüggvény példánya
         private static System.Timers.Timer aTimer;
-        static uint maxido = 0, ido=0;
-        TimeSpan workOut, SworkOut= TimeSpan.FromSeconds(0);
-        static uint idoCheck = 100000, limit = 900000;
-        bool kiskepernyo = true;
-        int counter = 0;
+        static uint maxido = 0, ido=0; //a stopper idö és annak maximuma
+        TimeSpan workOut, SworkOut= TimeSpan.FromSeconds(0); //a az állással eltöltött részidö és ezek összege
+        static uint idoCheck = 100000, limit = 900000; //IdoCheck (ms) egy intervallum ami után frissülnek az adatok. limit elérése után az idöintervallum bekerül workOut-ba
+        bool kiskepernyo = true; //a Form kissebre-nagyobbra állításához
+       
         string inputFeld, loginfo, buttonIdeiglenes;
+        
         Font font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
         Label lbl;
-        DateTime startTime = DateTime.Now, endTime;
-        TimeSpan deltaEtap, deltaTread, halfMinute = TimeSpan.FromSeconds(25);
+
+        DateTime startTime = DateTime.Now, endTime;//idöpont
+        TimeSpan deltaEtap, deltaTread, halfMinute = TimeSpan.FromSeconds(25);//idötartam
        
         Screen scr;
 
         bool newActivity = false;
         string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\timestamp.txt";
+        
         StreamWriter sw;
+
+        private Point lastLocation;
+        private bool mouseDown;
 
         public Timestamp()
         {
@@ -36,12 +41,14 @@ namespace timestamp
             this.TopMost = true;
             this.StartPosition = FormStartPosition.Manual;
             scr = Screen.FromPoint(this.Location);
-            //this.Location = new Point(scr.WorkingArea.Right - this.Width - 20, scr.WorkingArea.Top);
             this.Location = new Point(scr.WorkingArea.Right/2, scr.WorkingArea.Bottom/2);
            
 
             label1.Text = ("New work:" + DateTime.Now.ToString("d"));
             label1.Font = font;
+            sw = new StreamWriter(path, true);
+            sw.WriteLine("\n\n" + label1.Text);
+            sw.Close();
 
             szamlalo();
         }
@@ -55,11 +62,6 @@ namespace timestamp
             // Hook up the Elapsed event for the timer.
             aTimer.Elapsed += OnTimedEvent;
 
-            // Have the timer fire repeated events (true is the default)
-            //aTimer.AutoReset = true;
-
-            // Start the timer
-            //aTimer.Enabled = true;
         }
 
         static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e) // ezt kell végrehajtania
@@ -71,6 +73,7 @@ namespace timestamp
                 maxido = ido;
             }
         }
+
         private void form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             endTime = DateTime.Now;
@@ -80,7 +83,7 @@ namespace timestamp
                 if (maxido >= limit)
                 {
                     workOut = TimeSpan.FromMilliseconds(maxido);
-                    deltaEtap = endTime.Subtract(startTime) - workOut + halfMinute;
+                    deltaEtap = endTime.Subtract(startTime) + halfMinute;
                     SworkOut = SworkOut + workOut;
                 }
                 else
@@ -94,9 +97,8 @@ namespace timestamp
             sw = new StreamWriter(path, true);
 
             sw.WriteLine("esc:" + endTime.ToString("HH:mm") + " The user has logged out " + deltaEtap.ToString(@"hh\:mm"));
-            sw.WriteLine("Work finished:" + "  -  Sum Time: " + deltaTread.ToString(@"hh\:mm"));
-            sw.WriteLine("Workout:" + "  -  Sum Time: " + SworkOut.ToString(@"hh\:mm"));
-
+            sw.WriteLine("Work finished:" + endTime.ToString("HH:mm") + "  Brutto Time: " + deltaTread.ToString(@"hh\:mm")+ " Summ Pause(which>15min):" + SworkOut.ToString(@"hh\:mm"));
+            
             sw.Close();
 
         }
@@ -104,12 +106,11 @@ namespace timestamp
         private void button1_Click(object sender, EventArgs e)
 
         {
-            //MessageBox.Show("ido:"+ido + " maxido: " + maxido + " Workout: " +  workOut);
             if (button1.Text=="Start")
             {
                 
-                aTimer.AutoReset = true;
-                aTimer.Enabled = true;
+                aTimer.AutoReset = true;// Have the timer fire repeated events (true is the default)
+                aTimer.Enabled = true; // Start the timer
                 newActivity = true;
                 button1.BackColor = Color.LightGreen;
                 button1.Text = "ongoing";
@@ -125,8 +126,6 @@ namespace timestamp
                 
                 workOut = TimeSpan.FromMilliseconds(maxido);
                 SworkOut = SworkOut + workOut;
-                maxido = 0;
-                workOut = TimeSpan.FromMilliseconds(maxido);
                 aTimer.Enabled = false;
                 newActivity = false;
                 button1.BackColor = Color.LightPink;
@@ -135,6 +134,8 @@ namespace timestamp
             }
             else 
             {
+                maxido = 0;
+                workOut = TimeSpan.FromMilliseconds(maxido);
                 aTimer.AutoReset = true;
                 aTimer.Enabled = true;
                 newActivity = true;
@@ -142,7 +143,6 @@ namespace timestamp
                 button1.Text = "ongoing";
                     
             }
-            //MessageBox.Show("ido:" + ido + " maxido: " + maxido + " Workout: " + workOut);
             Export(path);
             
         }
@@ -151,53 +151,31 @@ namespace timestamp
         {
            
             inputFeld = "";
-            
             loginfo = "";
 
             sw = new StreamWriter(path , true);
-            if (counter==0)
-            { 
-                sw.WriteLine("\n\n" + label1.Text);
-            }
+            
             if (newActivity)
             {
                 startTime = DateTime.Now;
-
                 InputBox(this, "new activity", "Description:", ref inputFeld);
-               
                 loginfo = "in :" + startTime.ToString("HH:mm") + "  " + inputFeld;
-               
-                
                 newActivity = false;
-                
             }
            
             else 
             {
-
                 endTime = DateTime.Now;
-
-                
-                
-                    deltaEtap = endTime.Subtract(startTime) + halfMinute;
-                  
-               
-                    deltaEtap = endTime.Subtract(startTime) + halfMinute;
-                
-                //InputBox(this, "stop activity", "Description:", ref inputFeld);
-               
+                deltaEtap = endTime.Subtract(startTime) + halfMinute;
+                deltaEtap = endTime.Subtract(startTime) + halfMinute;                         
                 deltaTread = deltaTread.Add(deltaEtap);
                 loginfo = "out:" + endTime.ToString("HH:mm") + "   " +
                     "\u0394:" + deltaEtap.ToString(@"hh\:mm") + " \u0394\u0394:" + deltaTread.ToString(@"hh\:mm") + "   Workout:" + workOut.ToString(@"hh\:mm");
-
             }
-                       
-           
+
             sw.WriteLine(loginfo);
             sw.Close();
             Felrak(loginfo);
-            counter++;
-
 
         }
 
@@ -218,8 +196,7 @@ namespace timestamp
            
         }
 
-        
-        public void Felrak(string loginfo)
+        public void Felrak(string loginfo) //a panel1-re felrakja az új tevékenységet leiró labelt
         {
 
             foreach (Control nagy in Controls)
@@ -240,7 +217,6 @@ namespace timestamp
             lbl.Text = loginfo;
             panel1.Controls.Add(lbl);
             panel1.Height= panel1.Height + 25;
-
 
         }
 
@@ -293,32 +269,23 @@ namespace timestamp
 
             return dialogResult;
         }
-        private bool mouseDown;
-
-  
 
         private void label2_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        private Point lastLocation;
-
-       
-
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            if (button1.Text=="ongoing")
+            buttonIdeiglenes = button1.Text;
+            if (maxido < limit)
             {
-                buttonIdeiglenes = button1.Text;
-                button1.Text = (DateTime.Now.Subtract(startTime).Add(deltaTread)).ToString(@"hh\:mm");
+                maxido = 0;
             }
-            else
-            {
-                buttonIdeiglenes = button1.Text;
-                button1.Text = (deltaTread).ToString(@"hh\:mm");
-            }
-            
+            TimeSpan kiesett = SworkOut + TimeSpan.FromMilliseconds(maxido);
+            TimeSpan elteltEtap = DateTime.Now.Subtract(startTime);
+            TimeSpan eredmeny = elteltEtap + deltaTread - kiesett;
+            button1.Text = (eredmeny).ToString();
             mouseDown = true;
             lastLocation = e.Location;
             this.Opacity = 1D;
